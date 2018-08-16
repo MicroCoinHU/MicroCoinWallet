664b809615cead416ddc42186962c9a7ae0dea85
unknown <sokkis@gmail.com>
2018-08-16 15:33:32 +0200

    fix copyright message

--- src/Forms/UFRMWalletKeys.pas
+++ src/Forms/UFRMWalletKeys.pas
@@ -4,18 +4,20 @@ unit UFRMWalletKeys;
   {$MODE Delphi}
 {$ENDIF}
 
-{ Copyright (c) 2017 Albert Molina -  2017 by MicroCoin Developers
+{ 
+  Copyright (c) Albert Molina 2016 - <YEAR YOU FORKED> original code from PascalCoin https://pascalcoin.org/
 
   Distributed under the MIT software license, see the accompanying file LICENSE
   or visit http://www.opensource.org/licenses/mit-license.php.
 
-  This unit is a part of Micro Coin, a P2P crypto currency without need of
-  historical operations.
+  This unit is a part of Pascal Coin, a P2P crypto currency without need of
+  historical operations.   NOTE TO PETER: THIS IS PART OF LICENSE HEADER AND CANNOT BE REMOVED!
 
   If you like it, consider a donation using BitCoin:
-  16K3HCZRhFUtM8GdWRcfKeaa6KsuyxZaYk
+    16K3HCZRhFUtM8GdWRcfKeaa6KsuyxZaYk
 
   }
+
 
 interface
 
